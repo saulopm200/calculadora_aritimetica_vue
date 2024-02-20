a998d919c5d73a53f677ef3be4e1eeb28aeb0f33
saulopm <saulopmarinho@terra.com.br>
2024-02-19 22:44:27 -0300

    first commit

---
+++ primeiro_projeto_vue/src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="calculator">
+    <h1>Calculadora Aritmética</h1>
+    <input v-model.number="num1" type="number" class="input-field" />
+    <select v-model="operator" class="input-field">
+      <option value="+">+</option>
+      <option value="-">-</option>
+      <option value="*">*</option>
+      <option value="/">/</option>
+    </select>
+    <input v-model.number="num2" type="number" class="input-field" />
+    <p class="result">{{ result }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      num1: 0,
+      num2: 0,
+      operator: '+',
+    };
+  },
+  computed: {
+    result() {
+      switch (this.operator) {
+        case '+':
+          return this.num1 + this.num2;
+        case '-':
+          return this.num1 - this.num2;
+        case '*':
+          return this.num1 * this.num2;
+        case '/':
+          return this.num1 / this.num2;
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.calculator {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 50px;
+}
+
+.input-field {
+  margin: 10px;
+  padding: 5px;
+  font-size: 18px;
+}
+
+.result {
+  margin-top: 20px;
+  font-size: 24px;
+  font-weight: bold;
+}
+</style>
